8f003a00eb3ade6d3e2db928e1cb51eb3c00cb3e
于海慧 <125227112@qq.com>
2019-07-18 17:52:29 +0800

    commit

--- src/components/tools/Logo.vue
+++ src/components/tools/Logo.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="logo" >
     <router-link :to="{name:'index'}">
-<<<<<<< HEAD
       <LogoSvg alt="logo" :style="{width:'120px',height:'100%'}" />
-=======
-      <LogoSvg alt="logo" />
->>>>>>> 2d8a84501b36fa5f81e224084b2722e371857bb2
       <!-- <h1 style="font-size:22px" v-if="showTitle">{{ title }}</h1> -->
     </router-link>
   </div>
