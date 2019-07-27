84e25f61485aa08cb8856936cd0b2ebf3e89fe7d
yuhaihui <yuhh@neuray.cn>
2019-07-27 16:57:21 +0800

    代码提交

--- src/config/defaultSettings.js
+++ src/config/defaultSettings.js
@@ -13,14 +13,37 @@
  *
  */
 
+// export default {
+//   primaryColor: '#52C41A', // primary color of ant design
+//   navTheme: 'dark', // theme for nav menu
+//   layout: 'sidemenu', // nav menu position: sidemenu or topmenu
+//   contentWidth: 'Fixed', // layout of content: Fluid or Fixed, only works when layout is topmenu
+//   fixedHeader: false, // sticky header
+//   fixSiderbar: false, // sticky siderbar
+//   autoHideHeader: false, //  auto hide header
+//   colorWeak: false,
+//   multiTab: false,
+//   production: process.env.NODE_ENV === 'production' && process.env.VUE_APP_PREVIEW !== 'true',
+//   iconfontUrl: '//at.alicdn.com/t/font_1305230_ew0b5j5enqn.js',
+//   defaultAvatar: require('../assets/icons/default-avatar.svg'),
+//   defaultErrorImg: require('../assets/errorImg.svg'),
+//   // vue-ls options
+//   storageOptions: {
+//     namespace: 'pro__', // key prefix
+//     name: 'ls', // name variable Vue.[ls] or this.[$ls],
+//     storage: 'local' // storage name session, local, memory
+//   }
+// }
+
+
 export default {
-  primaryColor: '#52C41A', // primary color of ant design
+  primaryColor: '#1890FF', // primary color of ant design
   navTheme: 'dark', // theme for nav menu
-  layout: 'sidemenu', // nav menu position: sidemenu or topmenu
+  layout: 'topmenu', // nav menu position: sidemenu or topmenu
   contentWidth: 'Fixed', // layout of content: Fluid or Fixed, only works when layout is topmenu
-  fixedHeader: false, // sticky header
+  fixedHeader: true, // sticky header
   fixSiderbar: false, // sticky siderbar
-  autoHideHeader: false, //  auto hide header
+  autoHideHeader: true, //  auto hide header
   colorWeak: false,
   multiTab: false,
   production: process.env.NODE_ENV === 'production' && process.env.VUE_APP_PREVIEW !== 'true',
@@ -29,28 +52,8 @@ export default {
   defaultErrorImg: require('../assets/errorImg.svg'),
   // vue-ls options
   storageOptions: {
-    namespace: 'pro__', // key prefix
-    name: 'ls', // name variable Vue.[ls] or this.[$ls],
-    storage: 'local' // storage name session, local, memory
+    namespace: 'pro__',
+    name: 'ls',
+    storage: 'local',
   }
 }
-
-
-// export default {
-//   primaryColor: '#1890FF', // primary color of ant design
-//   navTheme: 'dark', // theme for nav menu
-//   layout: 'topmenu', // nav menu position: sidemenu or topmenu
-//   contentWidth: 'Fixed', // layout of content: Fluid or Fixed, only works when layout is topmenu
-//   fixedHeader: true, // sticky header
-//   fixSiderbar: false, // sticky siderbar
-//   autoHideHeader: true, //  auto hide header
-//   colorWeak: false,
-//   multiTab: false,
-//   production: process.env.NODE_ENV === 'production' && process.env.VUE_APP_PREVIEW !== 'true',
-//   // vue-ls options
-//   storageOptions: {
-//     namespace: 'pro__',
-//     name: 'ls',
-//     storage: 'local',
-//   }
-// }
